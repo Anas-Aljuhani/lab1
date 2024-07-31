f163a862e51f85f26321d6839ac6fa16f53a0ade
anas <anas.aljuhani.dev@gmail.com>
2024-07-31 11:41:30 +0300

    new branch

--- bin/gittest.dart
+++ bin/gittest.dart
@@ -1,6 +1,9 @@
 import 'package:gittest/gittest.dart' as gittest;
 import 'gitprint.dart';
+import 'new_branch.dart';
 
 void main(List<String> arguments) {
   gitprint();
+  //cnew comment
+  newbranch();
 }
